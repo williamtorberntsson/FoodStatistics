ad52f6929a60aab148aa97dd30f8b64446c31262
William Torberntsson <william.torberntsson@gmail.com>
2024-02-28 20:53:19 +0100

    init page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Statistics from './Statistics'
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Main">
+      <Statistics />
+    </div>
+  );
+}
+
+export default App
---
+++ src/Statistics.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import data from "./tests.json";
+
+const Statistics = () => {
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    setTests(data.tests);
+  }, [])
+
+  const calculateCorrectness = (participant, test) => {
+    const correctGuesses = participant.guesses.filter((guess, index) => guess === test.truth[index]).length;
+    return (correctGuesses / test.truth.length) * 100;
+  };
+
+  return (
+    <div className="app-container"> {/* Apply a class for centering */}
+      {tests.map(test => (
+        <div key={test.name}>
+          <h2>{test.name}</h2>
+          <p>{test.description}</p>
+          <div className="alternatives">
+            <p>Alternatives:</p>
+            <ul>
+              {test.alternatives.map((alternative, index) => (
+                <li key={index}>{alternative}</li>
+              ))}
+            </ul>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Participant</th>
+                <th>Guesses</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Rows for participants */}
+              {test.participants.map(participant => (
+                <tr key={participant.name}>
+                  <td>{participant.name}</td>
+                  <td>{participant.guesses.map((guess, index) => (
+                    <span key={index}>{test.alternatives[guess]}{(index !== participant.guesses.length - 1) && ", "}</span>
+                  ))}
+                  </td>
+                </tr>
+              ))}
+              <tr key="GroundTruth">
+                <td>Ground Truth</td>
+                <td>{test.truth.map((result, index) => (
+                  <span key={index}>{test.alternatives[result]}{(index !== test.truth.length - 1) && ", "}</span>
+                ))}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="correctness">
+            <p>Correctness:</p>
+            <ul>
+              {test.participants.map(participant => (
+                <li key={participant.name}>
+                  {participant.name}: {calculateCorrectness(participant, test).toFixed(2)}%
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Statistics;
